572ecfa62472fbbb39cf82339d67e8176af56416
Dev Abir <abirabullah@gmail.com>
2022-12-08 09:48:44 +0200

    Initial commit

---
+++ app/src/main/java/ucas/edu/android/firebase2022/LoginActivity.java
@@ -0,0 +1,100 @@
+package ucas.edu.android.firebase2022;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+
+import ucas.edu.android.firebase2022.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    ActivityLoginBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        if(firebaseUser!=null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void register() {
+        String email = binding.editTextTextEmailAddress.getText().toString();
+        String password = binding.editTextTextPassword.getText().toString();
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // try {
+                        if(task.isSuccessful()){
+                            Log.d("LoginActivity",task.getResult().getUser().toString());
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                        }else{
+                            Log.d("LoginActivity",task.getException().getMessage());
+                            Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                        // }catch (Exception ex){
+                        // ex.printStackTrace();
+                        // }
+                        //else{
+                    }
+                    // }
+                });
+    }
+    private void login() {
+        String email = binding.editTextTextEmailAddress.getText().toString();
+        String password = binding.editTextTextPassword.getText().toString();
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // try {
+                        if(task.isSuccessful()){
+                            Log.d("LoginActivity",task.getResult().getUser().toString());
+
+                        }else{
+                            Log.d("LoginActivity",task.getException().getMessage());
+                            Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                        // }catch (Exception ex){
+                        // ex.printStackTrace();
+                        // }
+                        //else{
+                    }
+                    // }
+                });
+    }
+}
